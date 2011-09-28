a8d39d9b4a4b5f50ce7cf0e36b2be32cd6fd7538
A Flores <ctrl4ltdeleteme@gmail.com>
2011-09-28 14:29:27 +0200

    added a seeds file for new dev environments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+locs = Locale.create([
+  { :name => 'English', :code => 'en', :master => true  },
+  { :name => 'Spanish', :code => 'es', :master => false },
+  { :name => 'French',  :code => 'fr', :master => false }
+])
+
+locales = Locale.all
+
+translation_keys = TranslationKey.create([
+  { :scope => 'the.scope', :tkey => 'the_key'  },
+  { :scope => 'the.scope', :tkey => 'the_key1' }
+])
+
+translation_keys.each do |trans_key|
+  trans = Translation.create({ :value => 'some_value', :translation_key => trans_key, :locale => locs.first})
+  trans.update_attributes({ :master_translation => trans })
+end
